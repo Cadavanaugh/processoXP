24268cbe5150c9d980d2f043090b4a7d19d8eaa0
Jovane Castro <jovanecastro12@gmail.com>
2022-07-24 13:52:25 -0300

    :sparkles: feat: contaService
    Criada a service com as funções relacionadas à conta

---
+++ src/services/conta.js
@@ -0,0 +1,61 @@
+const {Conta} = require('../database/models');
+const Joi = require('joi')
+
+const contaService = {
+  getInfo: async (CodCliente) => {
+    const result = await Conta.findByPk(CodCliente)
+    return result;
+  },
+  saque: async (CodCliente, Valor) => {
+    const {Saldo: antigoValor} = await Conta.findByPk(CodCliente)
+    const saque = await Conta.update({Saldo: Number(antigoValor) - Number(Valor)}, {where: {CodCliente}})
+    return {
+      CodCliente,
+      Saldo: Number(antigoValor) - Number(Valor)
+    }
+  },
+  deposito: async (CodCliente, Valor) => {
+    const {Saldo: antigoValor} = await Conta.findByPk(CodCliente)
+    const deposito = await Conta.update({Saldo: Number(antigoValor) + Number(Valor)}, {where: {CodCliente}})
+    return {
+      CodCliente,
+      Saldo: Number(antigoValor) + Number(Valor)
+    }
+  },
+  validateSaque: async (body) => {
+    const {Saldo} = await Conta.findByPk(body.CodCliente)
+
+    const schema = Joi.object({
+      CodCliente:Joi.number().integer().positive().required(),
+      Valor:Joi.number().integer().positive().max(Number(Saldo)).required()
+    })
+
+    const {error, value} = schema.validate(body)
+
+    if(error) {
+      error.message = error.details[0].message
+      error.code = 400;
+      throw error
+    }
+
+    return value
+  },
+  validateDeposito: async (body) => {
+    const schema = Joi.object({
+      CodCliente:Joi.number().integer().positive().required(),
+      Valor:Joi.number().integer().positive().required()
+    })
+
+    const {error, value} = schema.validate(body)
+
+    if(error) {
+      error.message = error.details[0].message
+      error.code = 400;
+      throw error
+    }
+
+    return value
+  }
+}
+
+module.exports = contaService
